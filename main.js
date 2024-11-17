0b138b26cfea9dd0c2994fafa219333d3babfa46
Xolani <mchunuxolani@outlook.com>
2024-11-17 15:30:27 -0800

    Download-function commit

--- main.js
+++ main.js
@@ -13,3 +13,34 @@
  more.addEventListener("click",() =>{
      prt.classList.toggle("active")
  })
+
+ "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js">
+   
+  
+         function downloadImage() {
+             axios({
+                 url: 'Xolani CV.pdf',
+                 method: 'GET',
+                 responseType: 'blob'
+             })
+                 .then((response) => {
+                     if (response.status === 200) {
+                         const url = 
+                             window.URL.createObjectURL
+                             (new Blob([response.data]));
+                         const link = document.createElement('a');
+                         link.href = url;
+                         link.setAttribute('download', 'Xolani CV.jpg');
+                         document.body.appendChild(link);
+                         link.click();
+                         document.body.removeChild(link);
+                         window.URL.revokeObjectURL(url);
+                     } else {
+                         console.error('Error: Received non-200 status code');
+                     }
+                 })
+                 .catch((error) => {
+                     console.error('Error downloading the image:', error);
+                 });
+         }
+     
